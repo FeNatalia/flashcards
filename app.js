684c5d43cb933890c06b5b7ee5df70bbe6b2fc6a
Natalia Fedorova <nataliafedorova1989@gmail.com>
2022-01-07 15:21:34 +0100

    edit pages, add redirections, cookies, logout button

--- app.js
+++ app.js
@@ -1,17 +1,48 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const { request, response } = require("express");
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.set("view engine", "pug");
 
 app.get("/", (request, response) => {
-  response.render("index");
+  const name = request.cookies.username;
+  if (name) {
+    response.render("index", { name });
+  } else {
+    response.redirect("/hello");
+  }
 });
 
 app.get("/cards", (request, response) => {
   response.render("card", {
     prompt: "What is the capital of Sweden?",
   });
+});
+
+app.get("/hello", (request, response) => {
+  const name = request.cookies.username;
+  if (name) {
+    response.redirect("/");
+  } else {
+    response.render("hello");
+  }
+});
+
+app.post("/hello", (request, response) => {
+  response.cookie("username", request.body.username);
+  response.redirect("/");
+  //response.json(request.body);
+});
+
+app.post("/goodbye", (request, response) => {
+  response.clearCookie("username");
+  response.redirect("/hello");
 });
 
 app.listen(3000, () => {
